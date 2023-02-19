7f13ceafe78a983777c68b3f15c7e612572d9a6f
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-02-19 09:47:30 -0400

    feat: criado as models e relacionamentos

---
+++ app/Models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'short_description',
+        'long_description',
+        'price',
+        'comparative_price'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
+
+    public function sku(){
+        return $this->hasOne(Sku::class);
+    }
+}
